831ae2850150f8c8913d68783b40db1610752126
Monius <zy421741276@hotmail.com>
2018-05-12 22:47:15 +0800

    V1.0 bug track

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ app = create_app()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Ngrok': Ngrok}
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__': 
+    app.run(debug=True)        
